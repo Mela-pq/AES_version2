884242fe3e4ad61d3ef56773261cd756f69c7c2c
Mela-pq <melqui36927@gmail.com>
2026-02-09 16:31:58 -0500

    Implementacion de funciones

--- Parte2.py
+++ Parte2.py
@@ -1,3 +1,35 @@
+#Funciones
+
+#Para saber cuantas filas y 
+# columnas tiene la matriz.
+
+def dimension (matriz):
+    filas = len(matriz)
+    if filas > 0:
+        columnas = len(matriz[0])
+    else:
+        columnas = 0
+    return filas, columnas
+
+#manera de imprimir  con un diseño .
+
+def imprimir (A):
+    f, c = dimension (A)
+    for i in range(0, f):
+        print ("| ", end = "") # end = "" es para no imprimir un salto de línea al final del print
+        for j in range(0, c):
+            print ("{:>5} |".format(A[i][j]), end = "")
+        print("") #Imprime un salto de línea
+
+#matriz llena de ceros
+
+def inicializar (nf, nc):
+    A = [] #Lista vacía
+    for i in range (nf):
+        fila = [0] * nc # nueva fila
+        A.append(fila) # aumente una copia de la fila            
+    return A
+
 #ShiftRows: Desplazamiento cíclico de las filas de la matriz.
 #cada matriz se desplaza a la izquierda 
 #cada fila se mueve una cantidad diferente
@@ -13,11 +45,12 @@
 #fila[i:]+ fila[:i] i es el numero o la cantidad
 #de movimientos y permite cambiar el orden de la matriz
 
+
 def shiftRows (m):
     
     nueva_matriz=[]
     
-    for i in range(len(m)):
+    for i in range(4):
         
         fila= m[i]
         
